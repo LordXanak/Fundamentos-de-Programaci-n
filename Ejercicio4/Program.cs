85f3d3f80a0fc204bcf690e5db975ead9230d0a7
LordXanak <61519097+LordXanak@users.noreply.github.com>
2020-02-26 18:02:05 -0400

    Ejercicio4

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 50; a++)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
