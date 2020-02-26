b4bb79f7ac305660e7dc4d0868762ff3bdbe131c
LordXanak <61519097+LordXanak@users.noreply.github.com>
2020-02-26 17:41:32 -0400

    Ejercicio2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba su nombre");
+            String nombre = Console.ReadLine();
+ 
+            Console.WriteLine("Escriba su edad");
+            String edad = Console.ReadLine();
+
+           
+
+            Console.WriteLine("Te llamas " + nombre + " y tienes " + edad + " años");
+
+            Console.ReadLine();
+
+        }
+    }
+}
